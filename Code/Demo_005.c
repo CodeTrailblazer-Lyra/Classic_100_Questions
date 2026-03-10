90cdc90518d5dccc9df29839396af49a8a0854f0
JunyeZhou <1801517241@qq.com>
2026-03-10 23:55:54 +0800

    add Demo_005.c and Demo_006.c

--- Code/Demo_005.c
+++ Code/Demo_005.c
@@ -1,3 +1,25 @@
-//
-// Created by JunyeZhou on 2026/3/10.
-//
+// C 练习实例5 - 三个数由小到大
+// 题目：输入三个整数 x、y、z，请把这三个数由小到大输出。
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int main() {
+    int x, y, z;
+
+    printf("输入三个整数 x,y,z（用逗号分隔）:\n");
+    scanf("%d,%d,%d", &x, &y, &z);
+
+    // 使用冒泡排序思想进行三数排序
+    if (x > y) swap(&x, &y);
+    if (x > z) swap(&x, &z);
+    if (y > z) swap(&y, &z);
+
+    printf("从小到大排序结果: %d %d %d\n", x, y, z);
+    return 0;
+}
--- Code/Demo_006.c
+++ Code/Demo_006.c
@@ -1,3 +1,13 @@
-//
-// Created by JunyeZhou on 2026/3/10.
-//
+// C 练习实例6 - 输出字母C图案
+// 题目：用*号输出字母C的图案。
+
+#include<stdio.h>
+
+int main() {
+    printf("用 * 号输出字母 C!\n");
+    printf("  ****\n");
+    printf(" *\n");
+    printf("*\n");
+    printf(" *\n");
+    printf("  ****\n");
+}
